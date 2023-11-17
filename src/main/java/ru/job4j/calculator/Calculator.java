396f016c42d6b0c80b6d48e2beb6c18cc61da27d
plinio <avzhykov@gmail.com>
2023-11-17 23:17:17 +0600

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -8,8 +8,7 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-        Calculator.plus(100, 500);
-        Calculator.plus(4, 2);
-        Calculator.plus(3, 5);
+        Calculator.plus(1, 2);
+        Calculator.plus(10, 11);
     }
 }
