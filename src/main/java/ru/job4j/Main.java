8828559c54f8181e2c3e88bba33169e042a69552
plinio <avzhykov@gmail.com>
2023-11-17 23:01:26 +0600

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,12 +2,13 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        short age;
-        char gender;
-        float height;
-        int countBricks;
-        double countBacteria;
-        boolean workPermission;
-        long size;
+        short age = 39;
+        char gender = 'М';
+        float height = 82.5F;
+        int countBricks = 800;
+        double countBacteria = 8.5E10;
+        long distance = 9_460_730_472_581L;
+        boolean workPermission = true;
+        long size = 1_000_000;
     }
 }
