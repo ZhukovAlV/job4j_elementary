73093eca5f3c877ed993f7909bc3d8d01d81e542
plinio <avzhykov@gmail.com>
2023-11-18 23:13:22 +0600

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        var idea = "I like Java!";
+        System.out.println(idea);
+        idea += "But I am a newbie.";
+        var year = 2023;
+        idea += year;
+        System.out.println(idea);
+    }
+
+}
