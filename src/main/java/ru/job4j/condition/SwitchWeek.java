72316df21bc7d55b36b21c718da7bb55cf947b5e
plinio <avzhykov@gmail.com>
2023-11-20 10:54:06 +0600

    4.6.1. Switch [#334070 #461156]

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -3,16 +3,33 @@ package ru.job4j.condition;
 public class SwitchWeek {
 
     public static String nameOfDay(int day) {
-        return switch (day) {
-            case 1 -> "Понедельник";
-            case 2 -> "Вторник";
-            case 3 -> "Среда";
-            case 4 -> "Четверг";
-            case 5 -> "Пятница";
-            case 6 -> "Суббота";
-            case 7 -> "Воскресенье";
-            default -> "Ошибка";
-        };
+        String name;
+        switch (day) {
+            case 1:
+                name = "понедельник";
+                break;
+            case 2:
+                name = "вторник";
+                break;
+            case 3:
+                name = "среда";
+                break;
+            case 4:
+                name = "четверг";
+                break;
+            case 5:
+                name = "пятница";
+                break;
+            case 6:
+                name = "суббота";
+                break;
+            case 7:
+                name = "воскресенье";
+                break;
+            default:
+                name = "неизвестный день";
+        }
+        return name;
     }
 
 }
